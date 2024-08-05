68b132f89d2569428400cc2bbcc8230830022167
BryanBahala <bryan.bahala@med4care.online>
2024-08-05 15:03:39 +0800

    update 2

--- routes/api.php
+++ routes/api.php
@@ -13,31 +13,34 @@ use Illuminate\Http\Request;
 |
 */
 
-
-
-Route::group(['namespace' => '\core'], function () {
-    foreach (scandir($basePath = base_path('core')) as $dir) {
-        $dirPath = "{$basePath}/{$dir}";
-        if ($dir === 'guest') {
-            foreach (scandir($dirPath) as $guestModule) {
-                Route::group(['prefix' => $dir, 'middleware' => $dir], function () use ($dirPath, $guestModule) {
-                    if (file_exists($routeFile = "{$dirPath}/{$guestModule}/routes/route.php")) {
-                        include $routeFile;
-                    }
-                });
-            }
-        } else {
-            foreach (scandir($dirPath) as $module) {
-                Route::group(['prefix' => strtolower($dir), 'middleware' => 'auth:api'], function () use ($dirPath, $module) {
-                    if (file_exists($routeFile = "{$dirPath}/routes/route.php")) {
-                        include $routeFile;
-                    }
-
-                    if (file_exists($routeFile = "{$dirPath}/{$module}/routes/route.php")) {
-                        include $routeFile;
-                    }
-                });
-            }
-        }
-    }
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
 });
+
+
+// Route::group(['namespace' => '\core'], function () {
+//     foreach (scandir($basePath = base_path('core')) as $dir) {
+//         $dirPath = "{$basePath}/{$dir}";
+//         if ($dir === 'guest') {
+//             foreach (scandir($dirPath) as $guestModule) {
+//                 Route::group(['prefix' => $dir, 'middleware' => $dir], function () use ($dirPath, $guestModule) {
+//                     if (file_exists($routeFile = "{$dirPath}/{$guestModule}/routes/route.php")) {
+//                         include $routeFile;
+//                     }
+//                 });
+//             }
+//         } else {
+//             foreach (scandir($dirPath) as $module) {
+//                 Route::group(['prefix' => strtolower($dir), 'middleware' => 'auth:api'], function () use ($dirPath, $module) {
+//                     if (file_exists($routeFile = "{$dirPath}/routes/route.php")) {
+//                         include $routeFile;
+//                     }
+
+//                     if (file_exists($routeFile = "{$dirPath}/{$module}/routes/route.php")) {
+//                         include $routeFile;
+//                     }
+//                 });
+//             }
+//         }
+//     }
+// });
