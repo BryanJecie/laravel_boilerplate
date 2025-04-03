5c27a6e92e885cf98360f4494352f1337399d1bd
Bryan Bahala <bryanjeciebahala@gmail.com>
2025-04-03 11:42:48 +0800

    update index

---
+++ resources/js/backend/pages/users/components/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1>User List</h1>
+        <div v-if="loading">Loading...</div>
+        <div v-else-if="error">{{ error }}</div>
+        <ul v-else>
+            <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+        </ul>
+        <button @click.prevent="onClick">test</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            users: [],
+            loading: false,
+            error: null
+        }
+    },
+    methods: {
+        onClick() {
+            alert()
+        }
+    }
+}
+
+</script>
+
+
+<style scoped>
+
+</style>
